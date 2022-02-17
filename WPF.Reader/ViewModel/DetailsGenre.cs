3b224e38816d0713f4e03fb6a20ee54877f0d30a
Nicolas Meyer <nicolas.meyer@etu.unice.fr>
2022-02-17 12:09:52 +0100

    optional wpf

---
+++ WPF.Reader/ViewModel/DetailsGenre.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Toolkit.Mvvm.DependencyInjection;
+using System.ComponentModel;
+using System.Windows.Input;
+using WPF.Reader.Model;
+using WPF.Reader.Service;
+
+namespace WPF.Reader.ViewModel
+{
+    public class DetailsGenre : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Genre CurrentGenre { get; init; }
+
+        public DetailsGenre(Genre genre)
+        {
+            CurrentGenre = genre;
+        }
+    }
+
+}
--- WPF.Reader/ViewModel/ListGenre.cs
+++ WPF.Reader/ViewModel/ListGenre.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Toolkit.Mvvm.DependencyInjection;
+using System;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 using WPF.Reader.Model;
 using WPF.Reader.Service;
@@ -14,11 +17,19 @@ namespace WPF.Reader.ViewModel
         public ICommand ItemSelectedCommand { get; set; }
 
         // n'oublier pas faire de faire le binding dans ListBook.xaml !!!!
-        public ObservableCollection<Book> Books => Ioc.Default.GetRequiredService<LibraryService>().Books;
+        public ObservableCollection<Genre> Genres => Ioc.Default.GetRequiredService<LibraryService>().UpdateGenreList();
 
         public ListGenre()
         {
-            ItemSelectedCommand = new RelayCommand(book => { /* the livre devrais etre dans la variable book */ Ioc.Default.GetRequiredService<NavigationService>().Navigate<DetailsBook>(book); });
+            ItemSelectedCommand = new RelayCommand(genre => { /* the livre devrais etre dans la variable book */ Ioc.Default.GetRequiredService<NavigationService>().Navigate<DetailsGenre>(genre); });
         }
+
+        /*
+        private void ChangeText(object sender, RoutedEventArgs e)
+        {
+            ListGenre model = (sender as Button).DataContext as ListGenre;
+            //model.DynamicText = (new Random().Next(0, 100).ToString());
+        }
+        */
     }
 }
